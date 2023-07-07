58ed498b70dd185499171d3f9acfaf5fa7b4ce6d
krithikraju07 <97392551+krithikraju07@users.noreply.github.com>
2023-07-07 11:41:20 +0530

    Add files via upload

---
+++ ems/src/main/java/ems/web/UserServlet.java
@@ -0,0 +1,121 @@
+package ems.web;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+import jakarta.servlet.RequestDispatcher;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import ems.dao.*;
+import ems.model.*;
+
+
+
+@WebServlet("/")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private UserDAO userDAO;
+	
+	public void init() {
+		userDAO = new UserDAO();
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+
+		try {
+			switch (action) {
+			case "/new":
+				showNewForm(request, response);
+				break;
+			case "/insert":
+				insertUser(request, response);
+				break;
+			case "/delete":
+				deleteUser(request, response);
+				break;
+			case "/edit":
+				showEditForm(request, response);
+				break;
+			case "/update":
+				updateUser(request, response);
+				break;
+			default:
+				listUser(request, response);
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void listUser(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		List<User> listUser = userDAO.selectAllUsers();
+		request.setAttribute("listUser", listUser);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-list.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void showNewForm(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	private void showEditForm(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		int id = Integer.parseInt(request.getParameter("id"));
+		User existingUser = userDAO.selectUser(id);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("user-form.jsp");
+		request.setAttribute("user", existingUser);
+		dispatcher.forward(request, response);
+
+	}
+
+	private void insertUser(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+		User newUser = new User(name, email, country);
+		userDAO.insertUser(newUser);
+		response.sendRedirect("list");
+	}
+
+	private void updateUser(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+		int id = Integer.parseInt(request.getParameter("id"));
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String country = request.getParameter("country");
+
+		User book = new User(id, name, email, country);
+		userDAO.updateUser(book);
+		response.sendRedirect("list");
+	}
+
+	private void deleteUser(HttpServletRequest request, HttpServletResponse response) 
+			throws Exception {
+		int id = Integer.parseInt(request.getParameter("id"));
+		userDAO.deleteUser(id);
+		response.sendRedirect("list");
+
+	}
+
+}
+
